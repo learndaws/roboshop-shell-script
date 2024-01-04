16f75219a6b39c197ffccff88cb195550c300fd1
learndaws <learndaws@gmail.com>
2024-01-04 18:08:41 +0530

    mongodb-install

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -6,6 +6,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+DATE=$(date +%F-%T)
+
+LOG=/tmp/$0_$DATE.log
+
 VALIDATE()
 {
 if [ $1 != 0 ];
@@ -21,27 +25,27 @@ SUDO_CHECK=$(id -u)
 
 VALIDATE "$SUDO_CHECK" "FAILED-1: Please run with sudo access" "SUCCESS-1: You have sudo access"
 
-cp mongo.repo /etc/yum.repos.d/mongod.repo
+cp mongo.repo /etc/yum.repos.d/mongod.repo &>> $LOG
 
 VALIDATE "$?" "FAILED-2: Mongodb repo copying failed" "SUCCESS-2: Mongodb repo copying success"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>> $LOG
 
 VALIDATE "$?" "FAILED-3: Mongodb installation failed" "SUCCESS-3: Mongodb installation success"
 
-systemctl enable mongod
+systemctl enable mongod &>> $LOG
 
 VALIDATE "$?" "FAILED-4: Mongodb service enable failed" "SUCCESS-4: Mongodb service enable success"
 
-systemctl start mongod
+systemctl start mongod &>> $LOG
 
 VALIDATE "$?" "FAILED-5: Mongodb service start failed" "SUCCESS-5: Mongodb service start success"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOG
 
 VALIDATE "$?" "FAILED-6: Loopback address modification failed" "SUCCESS-6: Loopback address modification success"
 
-systemctl restart mongod
+systemctl restart mongod &>> $LOG
 
 VALIDATE "$?" "FAILED-7: Mongodb service restart failed" "SUCCESS-7: Mongodb service restart success"
 
