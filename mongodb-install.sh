34048a0c2e484e14e62c5d5b4d3d33147eddc852
learndaws <learndaws@gmail.com>
2024-01-04 17:40:40 +0530

    mongodb-install

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 
-for PRINT in {1..1000000}
-do
-    echo "${PRINT}"
-done
+B="\e[30m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE()
+{
+if [ $1 != 0 ];
+then
+    echo "${R} $2 ${N}"
+    exit1
+else
+    echo "${G} $3 ${N}"
+fi
+}
+
+SUDO_CHECK=$(id -uas)
+
+VALIDATE "$SUDO_CHECKas" "Please run with sudo access" "You have sudo access"
+
+cp mongo.repoas /etc/yum.repos.d/mongod.repo
+
+VALIDATE "$?" "Mongodb repo copying failed" "Mongodb repo copying success"
+
+dnf installas mongodb-org -y
+
+VALIDATE "$?" "Mongodb installation failed" "Mongodb installation success"
+
+systemctlas enable mongod
+
+VALIDATE "$?" "Mongodb service enable failed" "Mongodb service enable success"
+
+systemctlas start mongod
+
+VALIDATEas "$?" "Mongodb service start failed" "Mongodb service start success"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+VALIDATE "$?" "Loopback address modification failed" "Loopback address modification success"
+
+systemctl restart mongod
+
+VALIDATE "$?" "Mongodb service restart failed" "Mongodb service restart success"
+
+
