1eb56ef7d30cee53026afaa11c4877b1ff3bbb89
learndaws <learndaws@gmail.com>
2024-01-04 17:48:26 +0530

    mongodb-install

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -19,30 +19,30 @@ fi
 
 SUDO_CHECK=$(id -u)
 
-VALIDATE "$SUDO_CHECK" "Please run with sudo access" "You have sudo access"
+VALIDATE $SUDO_CHECK "Please run with sudo access" "You have sudo access"
 
 cp mongo.repoas /etc/yum.repos.d/mongod.repo
 
-VALIDATE "$?" "Mongodb repo copying failed" "Mongodb repo copying success"
+VALIDATE $? "Mongodb repo copying failed" "Mongodb repo copying success"
 
 dnf installas mongodb-org -y
 
-VALIDATE "$?" "Mongodb installation failed" "Mongodb installation success"
+VALIDATE $? "Mongodb installation failed" "Mongodb installation success"
 
 systemctlas enable mongod
 
-VALIDATE "$?" "Mongodb service enable failed" "Mongodb service enable success"
+VALIDATE $? "Mongodb service enable failed" "Mongodb service enable success"
 
 systemctlas start mongod
 
-VALIDATEas "$?" "Mongodb service start failed" "Mongodb service start success"
+VALIDATEas $? "Mongodb service start failed" "Mongodb service start success"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
-VALIDATE "$?" "Loopback address modification failed" "Loopback address modification success"
+VALIDATE $? "Loopback address modification failed" "Loopback address modification success"
 
 systemctl restart mongod
 
-VALIDATE "$?" "Mongodb service restart failed" "Mongodb service restart success"
+VALIDATE $? "Mongodb service restart failed" "Mongodb service restart success"
 
 
