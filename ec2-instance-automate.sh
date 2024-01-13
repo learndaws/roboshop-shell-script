a2c073cd2c1fe4a16f6225ed32af84c72e888c72
learndaws <learndaws@gmail.com>
2024-01-13 14:43:23 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -2,5 +2,5 @@
 
 INSTANCE_NAME=("WEB-Server" "API-Catalogue" "API-Cart" "API-User" "API-Shipping" "API-Payments" "API-Ratings" "DB-Mongo" "DB-Redis" "DB-Mysql" "DB-Rabit MQ")
 
-echo "ALL INSTANCES: $INSTANCE_NAME[$@]"
+echo "ALL INSTANCES: $INSTANCE_NAME[@]"
 
