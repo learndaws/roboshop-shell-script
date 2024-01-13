ce1e527ea38f9d84d82a8a12e22465751ffd95fa
learndaws <learndaws@gmail.com>
2024-01-13 16:49:28 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -7,17 +7,19 @@ for line in "${INSTANCE_NAME[@]}"
 do 
     if [ "$line" == "DB-Mongo" ] || [ "$line" == "DB-Mysql" ] || [ "$line" == "API-Shipping" ];
     then 
-        echo "aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]""
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
     else 
-        echo "aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]""
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
     fi
 
     FIND_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$line" --query "Reservations[*].Instances[*].InstanceId" --output text)
     PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $FIND_INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
     
     
-    echo "$FIND_INSTANCE_ID"
-    echo "$PRIVATE_IP_ADDRESS"
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id Z08149982GBIICXQF76PI \
+        --change-batch "{"Changes":[{"Action":"CREATE","ResourceRecordSet":{"Name":"$line.hellodns.xyz","Type":"A","TTL":1,"ResourceRecords":[{"Value":"$PRIVATE_IP_ADDRES"}]}}]}"
+
 
 done
 
