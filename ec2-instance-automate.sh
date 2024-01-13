3c1b86d93e4860b15ab0a264bb66b16cc6630a54
learndaws <learndaws@gmail.com>
2024-01-13 20:15:55 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-INSTANCE_NAME=("web" "catalogue" "cart" "user" "shipping" "payments" "ratings" "mongodb" "redis" "mysql" "rabit-mq")
+INSTANCES_NAME=("web" "catalogue")
 
 
-for line in "${INSTANCE_NAME[@]}"
+for line in "${INSTANCES_NAME[@]}"
 do 
     if [ "$line" == "mongodb" ] || [ "$line" == "mysql" ] || [ "$line" == "shipping" ];
     then 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
+        INSTANCE_TYPE=t3.small
     else 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
+        INSTANCE_TYPE=t2.micro
     fi
+
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
 
     FIND_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$line" --query "Reservations[*].Instances[*].InstanceId" --output text)
     PRIVATE_IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $FIND_INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
@@ -18,26 +20,11 @@ do
 
     if [ "$line" == "web" ];
     then
-        aws route53 change-resource-record-sets \
-            --hosted-zone-id Z08149982GBIICXQF76PI \
-            --change-batch '{
-                "Changes": [
-                {
-                    "Action": "CREATE",
-                    "ResourceRecordSet": {
-                    "Name": "'"$line"'.hellodns.xyz",
-                    "Type": "A",
-                    "TTL": 1,
-                    "ResourceRecords": [
-                        {
-                        "Value": "'"$PUBLIC_IP_ADDRESS"'"
-                        }
-                    ]
-                    }
-                }
-                ]
-            }'
+        IP_ADDRESS_TYPE=$PUBLIC_IP_ADDRESS        
     else 
+        IP_ADDRESS_TYPE=$PRIVATE_IP_ADDRESS 
+
+    fi
         aws route53 change-resource-record-sets \
             --hosted-zone-id Z08149982GBIICXQF76PI \
             --change-batch '{
@@ -50,7 +37,7 @@ do
                     "TTL": 1,
                     "ResourceRecords": [
                         {
-                        "Value": "'"$PRIVATE_IP_ADDRESS"'"
+                        "Value": "'"$IP_ADDRESS_TYPE"'"
                         }
                     ]
                     }
@@ -58,6 +45,5 @@ do
                 ]
             }'
     fi
-
 done
 
