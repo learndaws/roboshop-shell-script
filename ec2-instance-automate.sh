c845964ecc5a4a3f8c8e36fc1cd94d6069776d16
learndaws <learndaws@gmail.com>
2024-01-13 17:06:10 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -17,24 +17,24 @@ do
     
     
     aws route53 change-resource-record-sets \
-  --hosted-zone-id Z08149982GBIICXQF76PI \
-  --change-batch "{
-    "Changes": [
-      {
-        "Action": "CREATE",
-        "ResourceRecordSet": {
-          "Name": "$line.hellodns.xyz",
-          "Type": "A",
-          "TTL": 1,
-          "ResourceRecords": [
-            {
-              "Value": "$PRIVATE_IP_ADDRES"
+      --hosted-zone-id Z08149982GBIICXQF76PI \
+      --change-batch '{
+        "Changes": [
+          {
+            "Action": "CREATE",
+            "ResourceRecordSet": {
+              "Name": "'"$line"'.hellodns.xyz",
+              "Type": "A",
+              "TTL": 1,
+              "ResourceRecords": [
+                {
+                  "Value": "'"$PRIVATE_IP_ADDRESS"'"
+                }
+              ]
             }
-          ]
-        }
-      }
-    ]
-  }"
+          }
+        ]
+      }'
 
 done
 
