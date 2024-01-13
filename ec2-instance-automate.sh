04373f727e606d583b6cd71c8080546af0725d93
learndaws <learndaws@gmail.com>
2024-01-13 14:46:09 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -1,6 +1,6 @@
 #\bin\bash
 
-FRUITS=("ORANGE" "GRAPES" "APPLE")
+FRUITS=("WEB-Server" "API-Catalogue" "API-Cart" "API-User" "API-Shipping" "API-Payments" "API-Ratings" "DB-Mongo" "DB-Redis" "DB-Mysql" "DB-Rabit MQ")
 
 echo "ALL FRUITS: ${FRUITS[@]}"
 
