cd6f1b3b569111f6df7a743c72d2ec6bc8675233
learndaws <learndaws@gmail.com>
2024-01-13 14:59:24 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -2,8 +2,8 @@
 
 INSTANCE_NAME=("WEB-Server" "API-Catalogue" "API-Cart" "API-User" "API-Shipping" "API-Payments" "API-Ratings" "DB-Mongo" "DB-Redis" "DB-Mysql" "DB-Rabit MQ")
 
-for i in "${INSTANCES_NAME[@]}"
+for line in "${INSTANCES_NAME[@]}"
 do 
-    echo "$i"
+    echo "$line"
 done
 
