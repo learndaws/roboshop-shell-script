b877a25f1c0ef12c2d0d6432851a531ab8c584bd
learndaws <learndaws@gmail.com>
2024-01-13 14:59:34 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -1,4 +1,4 @@
-#\bin\bash
+#!\bin\bash
 
 INSTANCE_NAME=("WEB-Server" "API-Catalogue" "API-Cart" "API-User" "API-Shipping" "API-Payments" "API-Ratings" "DB-Mongo" "DB-Redis" "DB-Mysql" "DB-Rabit MQ")
 
