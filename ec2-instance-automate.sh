f877f805659a45ea2e8960ed72bf49206253555b
learndaws <learndaws@gmail.com>
2024-01-13 15:18:17 +0530

    ec2-instance-automate

--- ec2-instance-automate.sh
+++ ec2-instance-automate.sh
@@ -4,10 +4,12 @@ INSTANCE_NAME=("WEB-Server" "API-Catalogue" "API-Cart" "API-User" "API-Shipping"
 
 for line in "${INSTANCE_NAME[@]}"
 do 
-    if [ $line == "DB-Mongo" || $line == "DB-Mysql" || $line == "API-Shipping" ];
+    if [ $line == "DB-Mongo" ] || [ $line == "DB-Mysql" ] || [ $line == "API-Shipping" ];
     then 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-01c0b35339d630515 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$line}]'
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
     else 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-01c0b35339d630515 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$line}]'
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-01c0b35339d630515 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$line}]"
     fi
 done
+
+
